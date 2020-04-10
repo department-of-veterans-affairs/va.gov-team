6e9129c316759f6ab44ce104cda08d7963f27e75
Shefali Nayak <shefali.nayak@thoughtworks.com>
2020-04-10 16:02:17 -0400

    [#71] adds all questions to nonsymptom-options.js

--- content/nonsymptom-messages.js
+++ content/nonsymptom-messages.js
@@ -2,6 +2,11 @@ scenario.messages = {
 
 root: `What do you need help with today?
 
-Please choose the topic that best fits your needs.`
+Please choose the topic that best fits your needs.
+You can always use the **Start over** button to start again and choose a different topic.`
+
+choose_question: `Please choose the question that best fits your needs.
+You can always ask another question if you need more information.`
+
 
 }
--- content/nonsymptom-options.js
+++ content/nonsymptom-options.js
@@ -5,6 +5,37 @@ root: [
   "VA's COVID response",
   "Mental health concerns",
   "Benefits and claims"
+],
+
+healthcare: [
+  "What should I do if I have an upcoming VA health appointment?",
+  "What should I do if I have a community care appointment?",
+  "Can I get a referral to see a community care provider if I can't be seen at VA?",
+  "How do I reschedule or cancel my upcoming VA appointment?"
+  "Can I use emergency care?",
+  "What should I do if I need to refill or renew my prescription?",
+  "What if I tried to call my clinic, and I can't get through?",
+  "How do I know if my VA health facility is still open for regular appointments?",
+  "Can I visit a patient at a VA facility?"
+  "Can I be be tested for coronavirus COVID-19 at VA?"
+],
+
+va_covid_response: [
+  "Can I be be tested for coronavirus COVID-19 at VA?",
+  "What is VA doing to deal with COVID-19?"
+],
+
+mental_health: [
+  "I don't have symptoms, but I feel anxious and overwhelmed. What can I do?",
+  "How can I ask for a new mental health appointment?"
+],
+
+benefits: [
+  "Will my benefit patient be delayed because of COVID-19?",
+  "My school changed all classes to online, Will I still qualify for my GI bill benefits?",
+  "Are VA regional offices closed?",
+  "How can I stay updated on regional office closures or service interruptions?",
+  "How can I contact my local regional office?"
 ]
 
 }
