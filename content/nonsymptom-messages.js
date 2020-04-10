1ec72f63950953beb5b0edf1955d3f692ad24843
Shefali Nayak <shefali.nayak@thoughtworks.com>
2020-04-10 18:02:43 -0400

    [#71] adds all question/answer strings to content JS files

--- content/nonsymptom-messages.js
+++ content/nonsymptom-messages.js
@@ -1,12 +1,198 @@
 scenario.messages = {
 
-root: `What do you need help with today?
+root: `
+What do you need help with today?
 
 Please choose the topic that best fits your needs.
-You can always use the **Start over** button to start again and choose a different topic.`
+You can always use the **Start over** button to start again and choose a different topic.
+`,
 
-choose_question: `Please choose the question that best fits your needs.
-You can always ask another question if you need more information.`
+choose_question: `
+Please choose the question that best fits your needs.
+You can always ask another question if you need more information.
+`,
 
+/* 12. What should I do if I have an upcoming VA health appointment? */
+upcoming_appointment: `
+##### For scheduled procedures
+
+If you have a non-urgent elective procedure scheduled, we may contact you to cancel or reschedule for a later date. Urgent and emergent procedues will continue as scheduled.
+
+##### For routine scheduled appointments
+
+We recommend using telehealth to visit your provider by phone or video.
+
+**To change your appointment to a telehealth visit, sign in online to:**
+- [Send a secure message](https:/*www.myhealth.va.gov/mhv-portal-web/web/myhealthevet/secure-messaging-spotlight) to your provider through My HealtheVet, **or**
+- [Request a telehealth appointment online]((https:/*www.va.gov/health-care/schedule-view-va-appointments/) for some types of appointments
+
+Once your provider schedules a telehealth appointment, you’ll receive a link to VA Video Connect (or another approved video meeting tool).
+
+You can also [cancel or reschedule your appointment](link to 20. How do I reschedule or cancel my upcoming VA appointment?).
+`,
+
+/* 17. Can I be tested for coronavirus COVID-19 at VA? */
+get_tested: `
+**This depends on many factors, including your:**
+- Symptoms and their severity
+- Other existing illnesses or conditions
+- Possible exposure
+
+Our health facilities have been testing Veterans who meet the CDC's criteria. We don't charge a fee for testing.
+`,
+
+/* 18. What should I do if I have a community care appointment? */
+community_care_apt: `
+
+Please contact your community care (non-VA) provider directly before going to your appointment. Some providers' hours or services may be affected by current CDC and local health department guidelines.
+
+##### If your community care provider cancels your appointment
+
+Work with the provider to reschedule. We're extending VA authorizations for community care referrals so you can continue to work with your provider to meet your needs during this time.
+
+If you have concerns about rescheduling your appointment, [send a secure message](https:/*www.myhealth.va.gov/mhv-portal-web/web/myhealthevet/secure-messaging-spotlight) to your VA provider.
+`,
+
+/* 19. What is VA doing to deal with COVID-19? */
+va_response: `
+We've activated our emergency management coordination cell (EMCC) and started clinical screenings at all VA facilities. <br>
+
+[Learn more about our public health response](https:/*www.publichealth.va.gov/n-coronavirus/)
+`,
+
+/* 20. How do I reschedule or cancel my upcoming VA appointment? */
+reschedule_apt: `
+Sign in to your VA.gov account to [reschedule or cancel your appointment online](https:/*www.va.gov/health-care/schedule-view-va-appointments/).
+
+**Note:** Some clinics are advising patients to not reschedule or schedule new appointments for some types of routine care, as they develop more guidance on safely providing care to protect patients and staff. Because this is a rapidly changing situation, the best way to get the most up-to-date facility information is to check your local VA health facility’s website. <br>
+[Find a VA location](https:/*www.va.gov/find-locations/)
+`,
+
+/* 21. Can I get a referral to see a community care provider if I can't be seen at VA? */
+referral: `
+This may depend on your needs. We’re continuing to make community care referrals when needed. But we’re prioritizing referrals for urgent or other medically necessary care. And some community care providers' hours or services may be affected CDC and local health department guidelines.
+
+**Please remember:** You need VA approval for all community care appointments, except for emergency and urgent care needs. Getting a referral depends on eligibility, type of care, and other factors. At this time, community care eligibility requirements haven't changed. <br>
+[Learn about eligibility for community care](https:/*www.va.gov/COMMUNITYCARE/programs/veterans/index.asp)
+`,
+
+/* 22. What if I tried to call my clinic and I can't get through? */
+cant_call_clinic: `
+We apologize if you can't get through or are on hold for a long time. We’re currently receiving a very high number of calls, and our staff is working hard to handle the increase.
+
+To help us address the most urgent needs first, we ask that you use one of our online tools for routine or non-urgent concerns. You can:
+
+- **Use Secure Messaging.** You can send a [secure message to your provider](https:/*www.myhealth.va.gov/mhv-portal-web/user-login?redirect=/mhv-portal-web/home) about any health concern or question.
+- **Try health chat (limited availability).** You may be able to use our health chat if your primary VA health facility is part of the VA health chat pilot. <br>
+[Find out if you're able to use health chat ](https:/*www.mobile.va.gov/app/va-health-chat)
+`,
+
+/* 23. What should I do if I need to refill or renew my prescription? */
+need_refill: `
+To make sure you have your medicine in time, request your refill or renewal at least 10 days before you’ll run out of your current prescription.
+
+##### If you currently get your prescription sent to you by mail
+You’ll continue to receive your refills as normal.
+
+##### If you usually pick up your prescription in person
+Use one of these other ways to get your medicine refilled or renewed.
+- **Online.** Use our [online prescription refill and tracking tool](https:/*www.myhealth.va.gov/mhv-portal-web/user-login?redirect=/mhv-portal-web/home) to refill or renew your prescription.
+- **By phone.** Call the number on your prescription label for your VA pharmacy. Be sure to have your RX number from the prescription label and Social Security number ready. For many prescriptions that are no longer refillable, this automated phone line may allow you to send a request to have more medication sent to you. For some medications, your health care team may contact you first.
+- **By mail.** Complete the prescription refill form for each medication, and mail it to your VA pharmacy. You'll find the pharmacy address on the paperwork that came with your prescription.
+
+##### If you have questions about your prescriptions
+Send a [secure message to your provider](https:/*www.myhealth.va.gov/mhv-portal-web/user-login?redirect=/mhv-portal-web/home) through My HealtheVet or call your local VA medical center.
+`,
+
+/* 24. Can I use emergency care? */
+emergency_care: `
+##### If you have life-threatening medical emergency
+
+Call 911 or go to your nearest ER. For emergencies, you don’t need a referral or approval from VA to go to a non-VA ER in your community.
+
+**Symtoms that may require emergency care include:**
+- Chest pain
+- Numbness or tingling in your arms or on one side of your body
+- Severe fever or violent vomiting
+- Bleeding that doesn’t stop
+
+##### If you have an urgent care need
+
+Please contact us first. You can use secure messaging or call your facility’s advice nurse before going to a clinic or walk-in urgent care facility. By not using the ER for nonmedical emergencies, you can help us protect you, medical staff, and other patients, as well as help us address critical medical needs first during this period of COVID-19 outbreak.
+
+**Symptoms that may require urgent care (within 24 to 48 hours) include:**
+- Sore throat, earache
+- Sprains or strained muscles from sports or exercise
+- Minor cuts and injuries
+`,
+
+/* 25. How do I know if my VA health facility is still open for regular appointments? */
+open_for_appointments: `
+Because this is a rapidly changing situation, the best way to get the most up-to-date facility information is to check your local VA health facility’s website. <br>
+[Find a VA location](https:/*www.va.gov/find-locations/)
+`,
+
+/* 26. I don't have symptoms, but I feel anxious and overwhelmed. What can I do? */
+feeling_anxious: `
+First, it's important to know you're not alone, and we're here for you. Concerns about COVID-19 can be stressful for many people. You can take steps to manage stress and get support.
+
+- **Stay connected and healthy.** Stay in touch with friends and family by phone and social media. Follow your normal routine as much as possible, and get enough sleep and exercise.
+- **Keep your existing mental health appointments.** If you currently have regular sessions, you can change them to telehealth (phone or video) appointments by [sending your provider a secure message](https:/*www.myhealth.va.gov/mhv-portal-web/user-login?redirect=/mhv-portal-web/home) through My HealtheVet.
+- **Ask for a new mental health appointment.** If you need same-day mental health care—or want to request a new appointment—call your local VA health facility.<br>
+**[Find a VA location](https:/*www.va.gov/find-locations)**
+- **[Get more VA tips for maintaining your mental health](https:/*www.mentalhealth.va.gov/coronavirus/)
+- **[Visit the CDC to learn more about managing COVID-19 stress and anxiety](https:/*www.cdc.gov/coronavirus/2019-ncov/daily-life-coping/managing-stress-anxiety.html?CDC_AA_refVal=https%3A%2F%2Fwww.cdc.gov%2Fcoronavirus%2F2019-ncov%2Fprepare%2Fmanaging-stress-anxiety.html)**
+`,
+
+/* 27. How can I ask for a new mental health appointment? */
+new_mental_health_apt: `
+_to be added_
+`,
+
+/* 28. Can I visit a patient at a VA facility? */
+visit_patient: `
+_to be added_
+`,
+
+/* 29. Will my benefit payment be delayed because of COVID-19? */
+benefit_delay: `
+We’re currently processing all benefit payments as normal. Benefit payments include payments for disability, pension, education payments to your school, and more.
+`,
+
+/* 30. My school changed all classes to online, Will I still qualify for my GI bill benefits? */
+gi_benefits: `
+Yes. If you’re currently receiving GI Bill benefits, payments will continue automatically. You don’t have to do anything. We know that many schools have changed to online classes in response to COVID-19.
+
+We’re working closely with schools to ensure enrollments are accurately certified and processed.
+
+If you have specific questions, you can contact the Education Call Center toll-free at 888-442-4551, Monday through Friday, 8:00 a.m. to 7:00 p.m. ET.
+`,
+
+/* 31. Are VA regional offices closed? */
+regional_office_closure: `
+Yes. We’ve temporarily closed all of our 56 regional offices for in-person visits, following current CDC guidelines to prevent community spread. But we can still support you with educational counseling, claims information, and other services by phone or approved video conferencing tools.
+
+**To get help or request a video appointment,** call us at 800-827-1000. We're here Monday through Friday, 8:00 a.m. to 9:00 p.m. ET.
+
+**To check on the status of your claim or appeal,** use our [online claim status tool](https:/*www.va.gov/claim-or-appeal-status/).
+
+**For other benefit questions,** send a question to our online customer service tool called [IRIS (Inquiry Routing & Information System)](https:/*iris.custhelp.va.gov/).
+`,
+
+/* 32. How can I stay updated on regional office closures or service interruptions? */
+stay_updated_closures: `
+For the latest status of our regional offices and other benefit services, you can:
+
+- Visit the [Veterans Benefits Administration (VBA) news page](https:/*www.benefits.va.gov/benefits/news.asp).
+- Call the benefits hotline toll-free at 800-827-1000.
+- Follow us on social media.
+  - [VBA Facebook](https:/*www.facebook.com/VeteransBenefits)
+  - VBA Twitter: [@VAVetBenefits](https:/*twitter.com/VAVetBenefits)
+`,
+
+/* 33. How can I contact my local regional office? */
+contact_regional_office: `
+Call us toll-free at 800-827-1000, 8:00 a.m. to 9:00 p.m. ET. Our regional office staff are still providing benefits help by phone and video.
+`
 
 }
--- content/nonsymptom-options.js
+++ content/nonsymptom-options.js
@@ -8,34 +8,73 @@ root: [
 ],
 
 healthcare: [
-  "What should I do if I have an upcoming VA health appointment?",
-  "What should I do if I have a community care appointment?",
-  "Can I get a referral to see a community care provider if I can't be seen at VA?",
-  "How do I reschedule or cancel my upcoming VA appointment?"
-  "Can I use emergency care?",
-  "What should I do if I need to refill or renew my prescription?",
-  "What if I tried to call my clinic, and I can't get through?",
-  "How do I know if my VA health facility is still open for regular appointments?",
-  "Can I visit a patient at a VA facility?"
-  "Can I be be tested for coronavirus COVID-19 at VA?"
+  [
+    "What should I do if I have an upcoming VA health appointment?",
+    "What should I do if I have a community care appointment?",
+    "Can I get a referral to see a community care provider if I can't be seen at VA?",
+    "How do I reschedule or cancel my upcoming VA appointment?",
+    "Can I use emergency care?",
+    "What should I do if I need to refill or renew my prescription?",
+    "What if I tried to call my clinic, and I can't get through?",
+    "How do I know if my VA health facility is still open for regular appointments?",
+    "Can I visit a patient at a VA facility?",
+    "Can I be be tested for coronavirus COVID-19 at VA?",
+    "Start over"
+  ],
+  [
+    scenario.messages.upcoming_appointment,
+    scenario.messages.community_care_apt,
+    scenario.messages.referral,
+    scenario.messages.reschedule_apt,
+    scenario.messages.emergency_care,
+    scenario.messages.need_refill,
+    scenario.messages.cant_call_clinic,
+    scenario.messages.open_for_appointments,
+    scenario.messages.visit_patient,
+    scenario.messages.get_tested,
+  ]
 ],
 
 va_covid_response: [
-  "Can I be be tested for coronavirus COVID-19 at VA?",
-  "What is VA doing to deal with COVID-19?"
+  [
+    "Can I be be tested for coronavirus COVID-19 at VA?",
+    "What is VA doing to deal with COVID-19?",
+    "Start over"
+  ],
+  [
+    scenario.messages.get_tested,
+    scenario.messages.va_response
+  ]
 ],
 
 mental_health: [
-  "I don't have symptoms, but I feel anxious and overwhelmed. What can I do?",
-  "How can I ask for a new mental health appointment?"
+  [
+    "I don't have symptoms, but I feel anxious and overwhelmed. What can I do?",
+    "How can I ask for a new mental health appointment?",
+    "Start over"
+  ],
+  [
+    scenario.messages.feeling_anxious,
+    scenario.messages.new_mental_health_apt
+  ]
 ],
 
 benefits: [
-  "Will my benefit patient be delayed because of COVID-19?",
-  "My school changed all classes to online, Will I still qualify for my GI bill benefits?",
-  "Are VA regional offices closed?",
-  "How can I stay updated on regional office closures or service interruptions?",
-  "How can I contact my local regional office?"
+  [
+    "Will my benefit patient be delayed because of COVID-19?",
+    "My school changed all classes to online, Will I still qualify for my GI bill benefits?",
+    "Are VA regional offices closed?",
+    "How can I stay updated on regional office closures or service interruptions?",
+    "How can I contact my local regional office?",
+    "Start over"
+  ],
+  [
+    scenario.messages.benefit_delay,
+    scenario.messages.gi_benefits,
+    scenario.messages.regional_office_closure,
+    scenario.messages.stay_updated_closures,
+    scenario.messages.contact_regional_office
+  ]
 ]
 
 }
