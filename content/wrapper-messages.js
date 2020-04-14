fc75dc1c4f8f91754e994d7d6c99abf049f7780a
Shefali Nayak <shefali.nayak@thoughtworks.com>
2020-04-14 14:17:29 -0400

    [#42] Adds file to store wrapper message strings, including VA-specific endings

---
+++ content/wrapper-messages.js
@@ -0,0 +1,94 @@
+scenario.messages = {
+
+welcome: `Hello, I'm here as VA's virtual assistant to help answer your questions about the coronavirus COVID-19 and VA benefits and services. If I can't answer all your questions, I'll help you connect to a call center representative.`,
+
+emergencyWarning: `**If you or someone else has any of these emergency warning signs of COVID-19, please call 911 to get medical care right away:**
+
+- Trouble breathing
+- Persistent (continuing) pain or pressure in the chest
+- Bluish lips or face
+- New confusion (suddenly feeling confused)
+- Inability to arouse (trouble waking up or staying awake)`,
+
+beforeWeGetStarted: `**Before we get started, do you have any of the symptoms listed below?**
+
+- Fever
+- Cough
+- Shortness of breath`,
+
+cdcSymptomCheckerPreText: `The Centers for Disease Control and Prevention (CDC) designed this tool to help you make decisions about seeking appropriate medical care.`,
+
+cdcSymptomCheckerPostText: {
+  /* CDC msg0: Please make a selection so I can help give you care advice. */
+  msg0: `
+**[IF user receives CDC "Pease make a selection so I can help give you care advice" message]**
+
+If you'd prefer to get information about the coronavirus COVID-19 and VA health care, benefits, and services, you can start over and choose another topic.
+  `,
+
+  /* CDC msg1: Sounds like you are feeling ok */
+  msg1: `
+If you have more questions about your symptoms, we're here to help.
+
+**To help us address the most urgent needs first, we ask that you use one of our online tools for routine or non-urgent questions. You can:**
+
+- **[Send a secure message to your VA provider](https://www.myhealth.va.gov/mhv-portal-web/user-login?redirect=/mhv-portal-web/home)** about any health concern or question.
+- **[Try our new health chat app (limited availability)](https://www.mobile.va.gov/app/va-health-chat)** if your primary VA health facility is one of the [select locations](https://www.mobile.va.gov/app/va-health-chat) testing this new feature.
+
+If you'd like to talk to someone about your general COVID-19 questions, you can call MyVA311 at 844-698-2311.
+  `,
+
+  /* CDC msg2: Call 911 - You may be having a medical emergency. */
+  msg2: `
+**Remember:** For emergencies, you don’t need a referral or approval from VA to go to a non-VA ER in your community.
+  `,
+
+  /* CDC msg4: Urgent medical attention may be needed. Please go to the Emergency Department */
+  msg4: `
+[Find your nearest VA health facility with emergency care](https://www.va.gov/find-locations/)
+
+**Remember:** For emergencies, you don’t need a referral or approval from VA to go to a non-VA ER in your community.
+  `,
+
+  /* CDC msg7: Contact a healthcare provider in the long-term care facility where you live */
+  msg7: `
+If you have non-urgent questions, you can [send a secure message to your VA provider](https://www.myhealth.va.gov/mhv-portal-web/user-login?redirect=/mhv-portal-web/home) through My HealtheVet
+
+We're taking many precautions to help protect Veterans in our VA nursing homes. This include limiting outside visitors. <br>[Learn more about steps we’re taking to protect Veterans in nursing homes](https://www.va.gov/opa/pressrel/pressrelease.cfm?id=5400).
+  `,
+
+  /* CDC msg8: Stay home and take care of yourself. Call your provider if you get worse */
+  msg8: `
+**If you have questions for your VA provider, or would like to request a telehealth visit by phone or video, you can:**
+- [Send a secure message to your provider](https://www.myhealth.va.gov/mhv-portal-web/user-login?redirect=/mhv-portal-web/home) through My HealtheVet
+- [Try our new health chat app (limited availability)](https://www.mobile.va.gov/app/va-health-chat) if your primary VA health facility is one of the [select locations](https://www.mobile.va.gov/app/va-health-chat) testing this new feature
+- [Request a telehealth appointment online]((https://www.va.gov/health-care/schedule-view-va-appointments/) for some types of appointments
+
+**If you think you need urgent care (within 24 to 48 hours)**
+
+**Please contact us first.** You can send a [secure message to your provider](https://www.myhealth.va.gov/mhv-portal-web/user-login?redirect=/mhv-portal-web/home) or call your facility’s advice nurse before going to a clinic or walk-in urgent care facility. <br>
+[Find a VA location](https://www.va.gov/find-locations)
+
+Please don't go to the ER for nonmedical emergencies. This helps us protect you, other patients, and our staff from community spread of the coronavirus COVID-19 and address critical medical needs first during this time.
+  `,
+
+  /* CDC msg9: Stay home and take care of yourself in home isolation */
+  msg9: `
+**To contact your VA provider or request a telehealth visit by phone or video, you can:**
+- [Send a secure message to your provider](https://www.myhealth.va.gov/mhv-portal-web/user-login?redirect=/mhv-portal-web/home) through My HealtheVet
+- [Try our new health chat app (limited availability)](https://www.mobile.va.gov/app/va-health-chat) if your primary VA health facility is one of the [select locations](https://www.mobile.va.gov/app/va-health-chat) testing this new feature
+- [Request a telehealth appointment online]((https://www.va.gov/health-care/schedule-view-va-appointments/) for some types of appointments
+
+**If you decide to go to a VA facility for urgent care**
+
+**Please contact us first.** You can send a [secure message to your provider](https://www.myhealth.va.gov/mhv-portal-web/user-login?redirect=/mhv-portal-web/home) or call your facility’s advice nurse before going to a clinic or walk-in urgent care facility. <br>
+[Find a VA location](https://www.va.gov/find-locations)
+
+Please don't go to the ER for nonmedical emergencies. This helps us protect you, other patients, and our staff from community spread of the coronavirus COVID-19 and address critical medical needs first during this time.
+  `
+}
+
+};
+
+
+
