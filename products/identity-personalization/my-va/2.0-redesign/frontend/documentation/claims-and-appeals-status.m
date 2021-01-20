eacf9ab09846ff7df7fefd5e50eb1f4bc60b8514
Samara Strauss <34068740+Samara-Strauss@users.noreply.github.com>
2021-01-20 13:39:59 -0500

    Create claims-and-appeals-status.m

---
+++ products/identity-personalization/my-va/2.0-redesign/frontend/documentation/claims-and-appeals-status.m
@@ -0,0 +1,24 @@
+## Claims and appeals specs 
+
+![Claims and appeals examples](https://github.com/department-of-veterans-affairs/va.gov-team/blob/master/products/identity-personalization/my-va/2.0-redesign/frontend/images/Claim%20%26%20appeals%20status%20example.jpg)
+
+### When to show the claims and appeals section
+
+**Show**
+
+**Do not show**
+
+### States
+  
+**State 1: Multiple claims in progress**  
+
+![Multiple claims in progress](https://github.com/department-of-veterans-affairs/va.gov-team/blob/master/products/identity-personalization/my-va/2.0-redesign/frontend/images/Claim%20%26%20appeals%20status_multiple%20claims.jpg)
+
+**State 2: One claim in progress**
+
+![One claim in progress](https://github.com/department-of-veterans-affairs/va.gov-team/blob/master/products/identity-personalization/my-va/2.0-redesign/frontend/images/Claim%20%26%20appeals%20status_one%20claims.jpg)
+
+**State 3: No claims in progress**
+
+![No claims in progress](https://github.com/department-of-veterans-affairs/va.gov-team/blob/master/products/identity-personalization/my-va/2.0-redesign/frontend/images/Claim%20%26%20appeals%20status_no%20claims.jpg)
+
