dc514184e99562a5da4c54fe564cd9988178da90
Ian <harrison.ian.t@gmail.com>
2023-10-30 10:27:01 -0500

    update script

--- scripts/github-actions/create-cc-completed-ticket.js
+++ scripts/github-actions/create-cc-completed-ticket.js
@@ -1,1 +1,26 @@
-console.log('testing...');
+const { Octokit } = require('@octokit/rest');
+
+const {
+  GITHUB_REPOSITORY,
+  BOT_GITHUB_TOKEN: auth,
+} = process.env;
+
+const octokit = new Octokit({ auth });
+const [owner, repo] = GITHUB_REPOSITORY.split('/');
+
+async function createTicket() {
+  console.log('creating issue...');
+  try {
+    await octokit.rest.issues.create({
+      owner,
+      repo,
+      title: 'this is a test',
+      body: 'this was created from a github action and is only a test.'
+    })
+  } catch (error) {
+    console.log('ticket failed to create:', error);
+  }
+  
+}
+
+createTicket();
