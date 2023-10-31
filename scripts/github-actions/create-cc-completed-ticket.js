738077e33eb388ab0312d1188db1e0a30f340489
Ian <harrison.ian.t@gmail.com>
2023-10-31 15:09:40 -0500

    update script

--- scripts/github-actions/create-cc-completed-ticket.js
+++ scripts/github-actions/create-cc-completed-ticket.js
@@ -1,22 +1,22 @@
 const { Octokit } = require('@octokit/rest');
 
 const {
-  GITHUB_REPOSITORY,
+  // GITHUB_REPOSITORY,
   // BOT_GITHUB_TOKEN: auth,
   GITHUB_TOKEN: auth
 } = process.env;
 
 const octokit = new Octokit({ auth });
-const [owner, repo] = GITHUB_REPOSITORY.split('/');
+// const [owner, repo] = GITHUB_REPOSITORY.split('/');
 
 async function createTicket() {
   console.log('creating issue...');
   try {
-    const resp = await octokit.request({
-      owner,
-      repo,
-      issue_number: '68645'
-
+    const resp = await octokit.issues.create({
+      owner: 'department-of-veterans-affairs',
+      repo: 'va.gov-team',
+      title: 'this is a test',
+      body: 'this is a test'
     })
     console.log(resp);
   } catch (error) {
