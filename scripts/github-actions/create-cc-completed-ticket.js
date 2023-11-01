dfb8a79f1f81f6f4a84ad5be673f457ec02e92c0
Ian <harrison.ian.t@gmail.com>
2023-11-01 14:55:58 -0500

    tweak syntax

--- scripts/github-actions/create-cc-completed-ticket.js
+++ scripts/github-actions/create-cc-completed-ticket.js
@@ -31,7 +31,7 @@ async function getTeamInfo() {
     if (productName !== featureName && featureName) {
       titleInfo = `${titleInfo}/${featureName}`
     }
-    console.log(`titleInfo=${titleInfo} >> $GITHUB_OUTPUT`);
+    console.log(`"titleInfo=${titleInfo}" >> $GITHUB_OUTPUT`);
   } catch (error) {
     process.exitCode = 1;
   }
