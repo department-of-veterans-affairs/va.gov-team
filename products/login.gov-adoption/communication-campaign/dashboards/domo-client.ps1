d446259983b29e9aac0e53ab7b2eeeb078a6f7c5
sdickson-gt <40237919+sdickson-gt@users.noreply.github.com>
2025-05-14 12:50:09 -0400

    Add files via upload

---
+++ products/login.gov-adoption/communication-campaign/dashboards/domo-client.ps1
@@ -0,0 +1,113 @@
+﻿# Args are hard-coded until they don't need to be
+# pwsh domo-client.ps1
+
+
+# example
+# pwsh domo-client.ps1 "..\1 extracted\Domo Data\MHV Users with Modern Creds" "mhv-01272025" "68c58f8f-dc38-4634-a3de-41b70991821c"
+
+
+$outputPath = $args[0]
+$fileName = $args[1]
+$datasetId = $args[2]
+
+$clientID = "UPDATE"
+$clientSecret = "UPDATE"
+
+
+$today = (Get-Date).ToString("MMddyyyy")
+
+#$fileName = "mhvdata-$($today)"
+#$outputPath = "..\1 extracted\Domo Data\MHV Users with Modern Creds" 
+#$datasetId = "28736674-a1e7-401d-b26b-ac1fd8e69056"  # test small dataset
+#$datasetId = "0bf90b88-93c7-4b9f-a64f-9324de4561df"
+
+
+
+$outfile = "$($outputPath)\$($fileName).csv"
+$logFile = "$($outputPath)\$($fileName)-log.txt"
+
+
+$strToEncode = $clientID + ":" + $clientSecret
+$bytes = [System.Text.Encoding]::UTF8.GetBytes($strToEncode)
+$base64authz = [Convert]::ToBase64String($bytes)
+
+$scope = "data"
+
+# Create Access Token
+$accessTokenUrl = "https://api.domo.com/oauth/token?grant_type=client_credentials&scope=$($scope)"
+
+
+
+$exportDataUrl = "https://api.domo.com/v1/datasets/$($datasetId)/data?includeHeader=true"
+
+
+if (Test-Path $outfile) {
+  Remove-Item $outfile
+}
+
+Write-Host "Output Target: $($outfile)"
+
+#"Output Target: $($outfile)" >> $logFile
+
+
+
+$headers = @{
+    "Authorization" = "Basic $($base64authz)"
+    "Accept" = "application/json"
+}
+
+
+
+
+Write-Host "Getting Domo Access Token" -ForegroundColor Green
+
+try {
+    $response = Invoke-WebRequest -Uri $accessTokenUrl `
+        -Method Get `
+        -Headers $headers `
+}
+catch {
+    Write-Warning $Error[0]
+    $Error[0] >> $logFile
+    Exit 1
+}
+
+
+#$response.Content >> $logFile
+
+$responseJSON = ConvertFrom-Json -InputObject $response.Content
+
+$accessToken = $responseJSON.access_token
+
+Write-Host "Access Token $($accessToken)" -ForegroundColor Green
+
+$headers = @{
+    "Authorization" = "Bearer " + $accessToken
+    "Accept" = "text/csv"
+}
+
+
+Write-Host "Exporting Data from Domo" -ForegroundColor Green
+
+try {
+    $response = Invoke-WebRequest -Uri $exportDataUrl `
+        -Method Get `
+        -Headers $headers `
+}
+catch {
+    Write-Warning $Error[0]
+    $Error[0] >> $logFile
+    Exit 1
+}
+
+
+#Write-Host $response
+
+$file = [System.IO.FileStream]::new($outFile, [System.IO.FileMode]::Create)
+$file.write($response.Content, 0, $response.RawContentLength)
+$file.close()
+
+#$icns.Values | Export-Csv -Path $outfile
+#Copy-Item $outfileToday -Destination $outfileLatest
+
+Exit 0
---
+++ products/login.gov-adoption/communication-campaign/dashboards/ssoe-edl-splunk-client-v1.ps1
@@ -0,0 +1,167 @@
+﻿# pwsh C:\Users\vacodickss\ETL\scripts\ssoe-edl-splunk-client-v1.ps1 "C:\Users\vacodickss\ETL\1 extracted\IAM SSOe Data\TEST" List_of_Distinct_Users_by_CSP_Monthly_secid pwd
+
+
+$outputPath = $args[0]
+$queryName = $args[1]
+$passwordPlainText = $args[2]
+
+$username = "iam-api-3"
+
+#SHORT CIRCUIT - for Domo Workbench to upload a manually updated latest file
+#Exit 0
+
+
+$now = Get-Date
+$todayDashes = (Get-Date).ToString("MM-dd-yyyy")
+$todaySlashes = (Get-Date).ToString("MM-dd-yyyy")
+
+$outfileLatest = "$($outputPath)\$($queryName)-latest.csv"
+$outfileToday  = "$($outputPath)\$($queryName)-$($todayDashes).csv" 
+$logFile = "$($outputPath)\$($queryName)-log.txt"
+
+
+if (Test-Path $outfileLatest) {
+  Remove-Item $outfileLatest
+}
+
+Write-Host "Calling SSOe query: $queryName.  Up til now: $now Today: $todaySlashes"  -ForegroundColor Green
+Write-Host "Output Target: $($outfileLatest)"
+
+"Output Target: $($outfileLatest)" >> $logFile
+
+$uriHistory = "https://project-splunk-1-west-shc-nlb-2a90998e3472318c.elb.us-gov-west-1.amazonaws.com:8089/servicesNS/iam-api-3/iam_car/saved/searches/$($queryName)/history"
+#$uriHistory = "https://project-splunk-1-west-shc-nlb-2a90998e3472318c.elb.us-gov-west-1.amazonaws.com:8089/services/saved/searches/$($queryName)/history"
+
+"History URL: $($uriHistory)" >> $logFile
+
+# curl.exe -s -k -u iam-api-3:<password> https://project-splunk-1-west-shc-nlb-2a90998e3472318c.elb.us-gov-west-1.amazonaws.com:8089/servicesNS/-/-/search/v2/jobs/export -d search="| savedsearch Availability_CPS" -d output_mode=csv -o TEST.csv
+# curl.exe -s -k -u iam-api-3:<password> https://project-splunk-1-west-shc-nlb-2a90998e3472318c.elb.us-gov-west-1.amazonaws.com:8089/servicesNS/iam-api-3/iam_car/saved/searches/TEST2/history
+ 
+
+
+
+$pwdSecureString = ConvertTo-SecureString $passwordPlainText -AsPlainText -Force
+
+
+if (! $pwdSecureString) {
+    "ERROR: ConvertTo-SecureString call failed" >> $logFile
+    Exit 1
+}
+
+
+try {
+    $credential = New-Object System.Management.Automation.PSCredential($username, $pwdSecureString)
+}
+catch {
+    Write-Warning $Error[0]
+    $Error[0] >> $logFile
+    Exit 1
+}
+
+$uri = $uriHistory
+
+try {
+    $response = Invoke-WebRequest -Uri $uri -Credential $credential -Authentication Basic -SkipCertificateCheck
+}
+catch {
+    Write-Warning $Error[0]
+    $Error[0] >> $logFile
+    Exit 1
+}
+
+
+$response.Content >> $logFile
+
+$responseXML = [xml]$response.Content
+
+$uriResultsBase = ""
+
+
+# Be aware that EDL Splunk runs on GMT
+# mult jobs may exist.  
+Write-Host "Today: $($todayDashes)"
+$responseXML.feed.entry | ? { [datetime]::parseexact($_.published.split("T")[0], 'yyyy-MM-dd', $null).ToString('MM-dd-yyyy') -eq $todayDashes } | % { $uriResultsBase = $_.id }
+#$responseXML.feed.entry | ? { [datetime]::parseexact($_.published.split("T")[0], 'yyyy-MM-dd', $null).ToString('MM-dd-yyyy') -eq "10-03-2023" } | % { $uriResultsBase = $_.id }
+
+
+if (! $uriResultsBase) {
+    Write-Output $response.Content
+    Write-Output "Results of Search Job with today's date not found."
+    Exit 1
+}
+
+
+Write-Output $uriResultsBase
+$uriResultsBase >> $logFile
+
+$uri = "$($uriResultsBase)/results"
+
+Write-Output $uri
+$uri >> $logFile
+
+
+$count = 50000
+$offset = 0
+
+
+$firstPass = 1
+# following should be true based on call to history uri above
+while ($response.Content) {
+
+    Write-Output "count: $count offset: $offset"
+
+    $postParams = @{
+        "output_mode" = "csv"
+        "count" = $count
+        "offset" = $offset
+    }
+
+    $offset += $count
+
+    try {
+        $response = Invoke-WebRequest -Uri $uri `
+            -Body $postParams `
+            -Credential $credential `
+            -Authentication Basic `
+            -SkipCertificateCheck
+    }
+    catch {
+        Write-Warning $Error[0]
+        $Error[0] >> $logFile
+        Exit 1
+    }
+
+
+    if ($response.Content) {
+        if ($firstPass) {
+            $firstPass = 0
+
+            $outfile = $outfileToday
+
+            # update outfile if output contains fromDate and toDate
+            $row1 = $response.Content | ConvertFrom-Csv | Select -First 1
+
+            $fromDate = $row1[0].("fromDate")
+            $toDate = $row1[0].("toDate")
+            if ($fromDate -and $toDate) {
+                $fromDateObj = [DateTime]$fromDate
+                $toDateObj = [DateTime]$toDate
+
+                $fromShort = $fromDateObj.ToString("yyyyMMdd")
+                $toShort = $toDateObj.ToString("yyyyMMdd")
+
+                $outfile  = "$($outputPath)\$($queryName)-$($fromShort)-$($toShort).csv"
+            }
+
+            Write-Output "outfile: $outfile"
+
+            $response.Content | ConvertFrom-Csv | Export-Csv $outfile -NoTypeInformation
+        } else {
+            $response.Content | ConvertFrom-Csv | Export-Csv $outfile -NoTypeInformation -Append
+        }
+    }
+}
+
+Copy-Item $outfile -Destination $outfileLatest
+
+Exit 0
