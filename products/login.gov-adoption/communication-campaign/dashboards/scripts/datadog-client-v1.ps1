faa45d6c10f9aa5e16fcd917c5d65c04d51ba12a
dennison33 <3373007+dennison33@users.noreply.github.com>
2025-07-01 17:03:00 -0700

    added scripts from root to new subfolder

---
+++ products/login.gov-adoption/communication-campaign/dashboards/scripts/datadog-client-v1.ps1
@@ -0,0 +1,351 @@
+﻿# example
+# pwsh datadog-client-v1.ps1 "C:\Users\vacodickss\ETL\1 extracted\VA.gov Data\Mobile Users" week 1
+
+# 2/3/2025 Added support for new error type: "request_timeout(Request timeout)", "deadline_exceeded(The query timed out)"
+
+$outputPath = $args[0]
+$period = $args[1]
+$periodsBack = $args[2]
+
+
+$apiKey = "UPDATE"
+$applicationKey = "UPDATE"
+
+
+#SHORT CIRCUIT - for Domo Workbench to upload a manually updated latest file
+#Exit 0
+
+#TEST
+#$outputPath = "C:\Users\vacodickss\ETL\1 extracted\VA.gov Data\Mobile Users" 
+
+
+Write-Host "$period $periodsBack"
+if ($period -eq "week") {
+    $fromWeek = (get-date).AddDays(-7 * $periodsBack)
+    $fromDay = $fromWeek.AddDays(0 - $fromWeek.DayOfWeek)
+    $toDay = $fromDay.AddDays(7)
+} elseif ($period -eq "month") {
+    $fromMonth = (get-date).AddMonths(-$periodsBack)
+    $toMonth = (get-date).AddMonths(-($periodsBack-1))
+    $fromDay = $fromMonth.AddDays(0 - $fromMonth.Day + 1)
+    $toDay = $toMonth.AddDays(0 - $toMonth.Day + 1)
+} else {
+    Write-Host "time period not supported " $period
+    Exit 1
+}
+
+
+#TEST
+#$fromDay = (get-date)
+#$toDay = (get-date)
+
+$from = $fromDay.ToString("yyyy-MM-ddT00:00-00:00")
+$fromShort = $fromDay.ToString("yyyyMMdd")
+$fromSlashes = $fromDay.ToString("MM/dd/yyyy")
+$to = $toDay.ToString("yyyy-MM-ddT00:00-00:00")
+$toShort = $toDay.ToString("yyyyMMdd")
+$toSlashes = $toDay.ToString("MM/dd/yyyy")
+
+#TEST
+#$from = $fromDay.ToString("yyyy-MM-ddT00:00-00:00")
+#$to = $toDay.ToString("yyyy-MM-ddT01:00-00:00")
+
+Write-Host "$from $to"
+
+
+
+$fileName = "mobileUsers-$($period)-$($fromShort)-$($toShort)"
+$fileNameLatest = "mobileUsers-$($period)"
+$query = "service:vets-api AND @message_content:*SignInController*callback" 
+#$from = "2024-12-16T00:00:00-00:00"
+#$to =   "2024-12-18T00:00:00-00:00"
+
+#Write-Host $fileName
+
+
+$limit = 1000
+
+$icns = @{}
+
+$globals = @{
+    'totalFound' = 0
+    'totalUnique' = 0
+    'lastTimestamp' = ''
+}
+
+
+#$now = Get-Date
+#$todayDashes = (Get-Date).ToString("MM-dd-yyyy")
+
+$outfile = "$($outputPath)\$($fileName).csv"
+$outfileLatest = "$($outputPath)\$($fileNameLatest)-latest.csv"
+$logFile = "$($outputPath)\$($fileNameLatest)-log.txt"
+
+
+function Process-Response {
+    param (
+        $json
+    )
+ 
+    $json.data | foreach {
+        #$debugJson = ConvertTo-Json $_
+        #Write-Host $debugJson
+
+        $icn = $_.attributes.attributes.payload.icn
+        $csp = $_.attributes.attributes.payload.type
+        $ial = $_.attributes.attributes.payload.ial
+
+        if (($ial -eq 1) -or (! $icn -or ! $csp)) {
+             #$jsonString = ConvertTo-Json $_
+             #Write-Host "Unexpected: " $jsonString
+
+        } else {
+
+            $icncsp = "icn$($icn)csp$($csp)"
+            $timestamp = $_.attributes.timestamp
+
+
+        
+            if ($icncsp) {
+               $globals['totalFound']++
+               $globals['lastTimestamp'] = $timestamp
+               if (! $icns.ContainsKey($icncsp)) {
+                    $globals['totalUnique']++
+                    $icns[$icncsp] = @{}
+        	        $icns[$icncsp]['icn'] = $icn
+                    $icns[$icncsp]['csp'] = $csp
+               }
+
+               #Write-Host "Found ICN: $($globals['totalFound']) $($globals['totalUnique']) $icn $timestamp"
+            }
+        }
+    }
+}
+
+
+
+
+
+if (Test-Path $outfile) {
+  Remove-Item $outfile
+}
+
+
+if (Test-Path $outfileLatest) {
+  Remove-Item $outfileLatest
+}
+
+
+Write-Host "Calling DataDog query: $queryName.  From: $from To: $to"  -ForegroundColor Green
+Write-Host "Output Target: $($outfile)"
+
+"Output Target: $($outfile)" >> $logFile
+
+
+$uri = "https://api.ddog-gov.com/api/v2/logs/events/search"
+$headers = @{
+    "DD-API-KEY" = $apiKey
+    "DD-APPLICATION-KEY" = $applicationKey
+}
+
+#&cols=%40icn%2C%40payload.typestorage="flex_tier
+# &cols=%40icn%2C%40payload.type
+# query=signinservice signincontroller callback env:eks-prod @payload.client_id:vamobile @icn:*
+#       signinservice signincontroller callback env:eks-prod @payload.client_id:vamobile @icn:*
+#       service:vets-api AND @message_content:*SignInController*callback AND env:eks-prod 
+# `"storage_tier`": `"online-archives`"
+#     `"storage_tier`": `"flex_tier`"
+$body =@"
+{
+  `"filter`": {
+    `"indexes`": [
+      `"*`"
+    ],
+    `"query`": `"$query`",
+    `"from`": `"$from`",
+    `"to`": `"$to`",
+    `"storage_tier`": `"flex`"
+  },
+  `"page`": {
+    `"limit`": $limit
+  },
+  `"sort`": `"timestamp`"
+}
+"@
+
+#    `"storage_tier`": `"indexes`"
+#    `"storage_tier`": [`"indexes`", `"online-archives`"]
+
+
+
+Write-Host "calling DataDog API..." -ForegroundColor Green
+
+try {
+    $response = Invoke-WebRequest -Uri $uri `
+        -Method Post `
+        -Headers $headers `
+        -ContentType "application/json" `
+        -Body "$body"
+}
+catch {
+    Write-Warning $Error[0]
+    $Error[0] >> $logFile
+    Exit 1
+}
+
+
+#$response.Content >> $logFile
+
+$responseJSON = ConvertFrom-Json -InputObject $response.Content
+
+Process-Response $responseJSON
+$now = $((get-date).ToString('T'))
+$log = "$now Found: $($globals['totalFound']) $($globals['totalUnique']) $($globals['lastTimestamp'])"
+Write-Host  $log
+$log >> $logFile
+
+$cursor = $responseJSON.meta.page.after
+#Write-Host "Cursor: $cursor"
+
+
+$bodyWithCursor = ""
+if ($cursor) {
+    $bodyJSON = ConvertFrom-Json -InputObject $body
+    $bodyJSON.page | add-member -Name "cursor" -value $cursor -MemberType NoteProperty
+    $bodyWithCursor = ConvertTo-Json $bodyJSON 
+    #Write-Host "updated body with cursor: " $bodyWithCursor
+}
+
+while ($cursor) {
+
+    Write-Host "calling DataDog API..." -ForegroundColor Green
+
+    try {
+        $response = Invoke-WebRequest -Uri $uri `
+            -Method Post `
+            -Headers $headers `
+            -ContentType "application/json" `
+            -Body "$bodyWithCursor"
+    }
+    catch {
+        Write-Host "DataDog did not return resultset..." -ForegroundColor DarkYellow
+        Write-Warning $Error[0]
+        $Error[0] >> $logFile
+
+        # Note the $response object doesn't get updated apparently due to the error, so can't count on this, but we can use the last value of reset
+        <#
+        $rem = $response.Headers["X-RateLimit-Remaining"]
+        $res = $response.Headers["X-RateLimit-Reset"]
+        $summary = "Remaining (requests): $rem, Reset (sec): $res"
+        Write-Host "$summary"
+        #>
+
+        # sample errors at end of file
+        $responseJSON = ConvertFrom-Json -InputObject $Error[0]
+        $err = $responseJSON.errors[0]
+        $code = $responseJSON.code
+        Write-Host "code: $code, error: $err"
+
+        if (($err -eq "Too many requests") -or 
+            ($err -eq "resource_exhausted(RateLimited: Too many pending queries)") -or
+            ($err -eq "deadline_exceeded(Request timeout)") -or
+            ($err -eq "request_timeout(Request timeout)") -or
+            ($err -eq "deadline_exceeded(The query timed out)")) {
+            Write-Host "Sleeping $rateLimitReset" -ForegroundColor DarkYellow
+            $sleep = [double]"$($rateLimitReset).0"
+            Start-Sleep -Seconds $sleep
+            Continue
+        } else {
+            Write-Host "Unknown error. Exiting"
+            Exit 1
+        }
+    }
+
+    $rateLimit = $response.Headers["X-RateLimit-Limit"]
+    $rateLimitPeriod = $response.Headers["X-RateLimit-Period"]
+    $rateLimitRemaining = $response.Headers["X-RateLimit-Remaining"]
+    $rateLimitReset = $response.Headers["X-RateLimit-Reset"]
+    $rateLimitName = $response.Headers["X-RateLimit-Name"]
+
+    # rateLimitName:  logs_public_search_api
+    # rateLimit:      60 requests per period
+    # ratLimitPeriod: 60 seconds
+    #$summary = "Request Limit/Period: $rateLimit, Remaining: $rateLimitRemaining, Reset: $rateLimitReset, Name: $rateLimitName, Period (sec): $rateLimitPeriod"
+    $summary = "Remaining (requests): $rateLimitRemaining, Reset (sec): $rateLimitReset"
+
+    Write-Host "$summary"
+    $summary >> $logFile
+
+    #$response.Content >> $logFile
+
+    $responseJSON = ConvertFrom-Json -InputObject $response.Content
+
+    Process-Response $responseJSON
+    $now = $((get-date).ToString('T'))
+    $log = "$now Found: $($globals['totalFound']) $($globals['totalUnique']) $($globals['lastTimestamp'])"
+    Write-Host  $log
+    $log >> $logFile
+
+    $cursor = $responseJSON.meta.page.after
+
+    $bodyJSON.page.cursor = $cursor
+    $bodyWithCursor = ConvertTo-Json $bodyJSON 
+    #Write-Host "updated body with cursor: " $bodyWithCursor
+
+
+    #$cursor = 0
+}
+
+$icns.Values | Select-Object @{Name='fromDate';Expression={$fromSlashes}},@{Name='toDate';Expression={$toSlashes}},* | Export-Csv $outfile -NoTypeInformation
+#$icns.Values | Export-Csv -Path $outfile
+Copy-Item $outfile -Destination $outfileLatest
+
+Exit 0
+
+
+
+
+<#
+# can reproduce this with multiple clients
+{
+  "status": "error",
+  "code": 429,
+  "errors": [
+    "Too many requests"
+  ],
+  "statuspage": "https://status.ddog-gov.com",
+  "twitter": "http://twitter.com/datadogops",
+  "email": "support@datadoghq.com"
+}
+
+{
+  "errors": [
+    "Too many requests"
+  ]
+}
+
+{
+  "errors": [
+    "resource_exhausted(RateLimited: Too many pending queries)"
+  ]
+}
+
+{
+  "errors": [
+    "deadline_exceeded(Request timeout)"
+  ]
+}
+
+{
+  "errors": [
+    "request_timeout(Request timeout)"
+  ]
+}
+
+{
+  "errors": [
+    "deadline_exceeded(The query timed out)"
+  ]
+}
+
+#>
---
+++ products/login.gov-adoption/communication-campaign/dashboards/scripts/ssoe-splunk-client-v2.ps1
@@ -0,0 +1,155 @@
+﻿# v2 removes the hard-coded TO_DATE column
+
+# pwsh C:\Users\vacodickss\ETL\scripts\ssoe-splunk-client-v2.ps1 "C:\Users\vacodickss\ETL\1 extracted\IAM SSOe Data\Distinct by CSP" List_of_Distinct_Users_by_CSP_Weekly_secid 
+
+$outputPath = $args[0]
+$queryName = $args[1]
+$pwd = $args[2]
+
+#SHORT CIRCUIT - for Domo Workbench to upload a manually updated latest file
+#Exit 0
+
+
+$now = Get-Date
+$todayDashes = (Get-Date).ToString("MM-dd-yyyy")
+$todaySlashes = (Get-Date).ToString("MM-dd-yyyy")
+
+$outfileLatest = "$($outputPath)\$($queryName)-latest.csv"
+$outfileToday  = "$($outputPath)\$($queryName)-$($todayDashes).csv" 
+$logFile = "$($outputPath)\$($queryName)-log.txt"
+
+
+if (Test-Path $outfileLatest) {
+  Remove-Item $outfileLatest
+}
+
+Write-Host "Calling SSOe query: $queryName.  Up til now: $now Today: $todaySlashes"  -ForegroundColor Green
+Write-Host "Output Target: $($outfileLatest)"
+
+"Output Target: $($outfileLatest)" >> $logFile
+
+$uriHistory = "https://vaazviamses100.vha.med.va.gov:8089/servicesNS/apicar/search/saved/searches/$($queryName)/history"
+
+$username = "apicar"
+
+$password = ConvertTo-SecureString $pwd -AsPlainText -Force
+
+if (! $password) {
+    "ERROR: ConvertTo-SecureString call failed" >> $logFile
+    Exit 1
+}
+
+
+try {
+    $credential = New-Object System.Management.Automation.PSCredential($username, $password)
+}
+catch {
+    Write-Warning $Error[0]
+    $Error[0] >> $logFile
+    Exit 1
+}
+
+$uri = $uriHistory
+
+try {
+    $response = Invoke-WebRequest -Uri $uri -Credential $credential -Authentication Basic -SkipCertificateCheck
+}
+catch {
+    Write-Warning $Error[0]
+    $Error[0] >> $logFile
+    Exit 1
+}
+
+$response.Content >> $logFile
+
+$responseXML = [xml]$response.Content
+
+$uriResultsBase = ""
+
+
+#mult jobs may exist.  
+$responseXML.feed.entry | ? { [datetime]::parseexact($_.updated.split("T")[0], 'yyyy-MM-dd', $null).ToString('MM-dd-yyyy') -eq $todayDashes } | % { $uriResultsBase = $_.id }
+#$responseXML.feed.entry | ? { [datetime]::parseexact($_.updated.split("T")[0], 'yyyy-MM-dd', $null).ToString('MM-dd-yyyy') -eq "10-03-2023" } | % { $uriResultsBase = $_.id }
+
+
+if (! $uriResultsBase) {
+    Write-Output $response.Content
+    Write-Output "Results of Search Job with today's date not found."
+    Exit 1
+}
+
+
+Write-Output $uriResultsBase
+$uriResultsBase >> $logFile
+
+$uri = "$($uriResultsBase)/results"
+
+Write-Output $uri
+$uri >> $logFile
+
+
+$count = 50000
+$offset = 0
+
+
+$firstPass = 1
+# following should be true based on call to history uri above
+while ($response.Content) {
+
+    Write-Output "count: $count offset: $offset"
+
+    $postParams = @{
+        "output_mode" = "csv"
+        "count" = $count
+        "offset" = $offset
+    }
+
+    $offset += $count
+
+    try {
+        $response = Invoke-WebRequest -Uri $uri `
+            -Body $postParams `
+            -Credential $credential `
+            -Authentication Basic `
+            -SkipCertificateCheck
+    }
+    catch {
+        Write-Warning $Error[0]
+        $Error[0] >> $logFile
+        Exit 1
+    }
+
+
+    if ($response.Content) {
+        if ($firstPass) {
+            $firstPass = 0
+
+            $outfile = $outfileToday
+
+            # update outfile if output contains fromDate and toDate
+            $row1 = $response.Content | ConvertFrom-Csv | Select -First 1
+
+            $fromDate = $row1[0].("fromDate")
+            $toDate = $row1[0].("toDate")
+            if ($fromDate -and $toDate) {
+                $fromDateObj = [DateTime]$fromDate
+                $toDateObj = [DateTime]$toDate
+
+                $fromShort = $fromDateObj.ToString("yyyyMMdd")
+                $toShort = $toDateObj.ToString("yyyyMMdd")
+
+                $outfile  = "$($outputPath)\$($queryName)-$($fromShort)-$($toShort).csv"
+            }
+
+            Write-Output "outfile: $outfile"
+
+            $response.Content | ConvertFrom-Csv | Export-Csv $outfile -NoTypeInformation
+        } else {
+            $response.Content | ConvertFrom-Csv | Export-Csv $outfile -NoTypeInformation -Append
+        }
+    }
+}
+
+Copy-Item $outfile -Destination $outfileLatest
+
+Exit 0
